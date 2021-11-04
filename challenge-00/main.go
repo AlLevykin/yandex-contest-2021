59b696e24eee04c185a04512c0bc3476f3b2ce17
AlLevykin <a.a.levykin@gmail.com>
2021-11-04 09:53:04 +0500

    Тестовая задач

--- challenge-00/main.go
+++ challenge-00/main.go
@@ -1,8 +1,27 @@
 // challenge 00
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Print("challenge 00")
+	var s, j string
+
+	fmt.Scanf("%s", &s)
+	fmt.Scanf("%s", &j)
+
+	seen := map[rune]struct{}{}
+	for _, letter := range s {
+		seen[letter] = struct{}{}
+	}
+
+	result := 0
+	for _, stone := range j {
+		if _, ok := seen[stone]; ok {
+			result++
+		}
+	}
+
+	fmt.Println(result)
 }
