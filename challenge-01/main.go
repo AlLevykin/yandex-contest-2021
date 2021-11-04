3801e907519e9d29a3104f53e74b875093443a19
AlLevykin <a.a.levykin@gmail.com>
2021-11-04 09:53:29 +0500

    Задача 1

--- challenge-01/main.go
+++ challenge-01/main.go
@@ -1,8 +1,41 @@
 // challenge 01
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Print("challenge 01")
+	var n, x, k int
+	fmt.Scanf("%d %d %d", &n, &x, &k)
+
+	dls  := make([]int, n, n)
+	for i:=0; i<n; i++ {
+		fmt.Scan(&dls[i])
+	}
+
+	keys := make([]int, k)
+	for j:=0; j<k; j++ {
+		keys[j] = 9223372036854775807
+	}
+
+	msg := make(map[int]struct{})
+
+	for i:=0; i<n; i++ {
+		for j:=0; j<k; j++ {
+			v := dls[i] + j*x
+			if _, ok := msg[v]; !ok {
+				msg[v] = struct{}{}
+				if keys[k-1] > v {
+					keys[k-1] = v
+					sort.Ints(keys)
+				}	else {
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Printf("%d", keys[k-1])
 }
